3a704cd955103de82d2edee4074698d02e81bd14
aries043 <aries2_0423@naver.com>
2021-11-02 17:33:03 +0900

    Revert "Wait"
    This reverts commit 14564f54aa392ea1795e67671fdd1d03b556a34c.

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -6,6 +6,9 @@ int main()
     printf("First OSS Code\n");
     printf("Second OSS Code\n");
 
-    printf("메인 브랜치에 코드를 추가하였음")
+    for(int i = 0; i < 10; i++){
+        printf("Test 브랜치\n");
+    }
     return 0;
+    printf("메인 브랜치에 코드를 추가하였음")
 }
